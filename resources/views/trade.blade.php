9c78ebd299e5f1e74c55a919f209575dd8608e47
ijlik <ijlik.k5s@gmail.com>
2019-08-02 20:42:16 +0700

    hlbit bot v 1.0

--- resources/views/trade.blade.php
+++ resources/views/trade.blade.php
@@ -21,19 +21,19 @@
 
                     <div class="card-body">
                         <h5>Trade Action for market BTCUSD</h5>
-                        {{--<form id="form-market" action="" method="GET">--}}
-                            {{--<label>Select Market</label>--}}
-                            {{--<select class="form-control" name="pair" required onchange="submit()">--}}
-                                {{--<option value="btcusd" {{ isset($_GET['pair']) && $_GET['pair'] == 'btcusd'? 'selected':'' }}>BTCUSD</option>--}}
-                                {{--<option value="ethusd" {{ isset($_GET['pair']) && $_GET['pair'] == 'ethusd'? 'selected':'' }}>ETHUSD</option>--}}
-                                {{--<option value="bpinkusd" {{ isset($_GET['pair']) && $_GET['pair'] == 'bpinkusd'? 'selected':'' }}>BPINKUSD</option>--}}
-                            {{--</select>--}}
-                        {{--</form>--}}
-                        {{--<script>--}}
-                            {{--function submit() {--}}
-                                {{--document.getElementById('form-market').submit();--}}
-                            {{--}--}}
-                        {{--</script>--}}
+                        <form id="form-market" action="" method="GET">
+                            <label>Select Market</label>
+                            <select class="form-control" name="pair" required onchange="submit()">
+                                <option value="btcusd" {{ isset($_GET['pair']) && $_GET['pair'] == 'btcusd'? 'selected':'' }}>BTCUSD</option>
+                                <option value="ethusd" {{ isset($_GET['pair']) && $_GET['pair'] == 'ethusd'? 'selected':'' }}>ETHUSD</option>
+                                <option value="bpinkusd" {{ isset($_GET['pair']) && $_GET['pair'] == 'bpinkusd'? 'selected':'' }}>BPINKUSD</option>
+                            </select>
+                        </form>
+                        <script>
+                            function submit() {
+                                document.getElementById('form-market').submit();
+                            }
+                        </script>
                         <br>
                         <div class="row">
                             <div class="col-md-6">
